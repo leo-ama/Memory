949a490c07670e2e16f0d6547dc31abb47c76fec
leo-ama <leonardo.amadini@gmail.com>
2025-05-29 15:52:22 +0200

    emoji

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ document.addEventListener('DOMContentLoaded', () => {
     let seconds = 0;
     
     // Emoji da utilizzare come simboli delle carte
-    const symbols = ['🍎', '🍌', '🍒', '🍓', '🍋', '🥝', '🍉', '🥭'];
+    const symbols = ['🍎', '🍌', '🍒', '🍓', '🍋', '🥝', '🍉', '🥭','😊','😂','👌','❤️','😒','😁','🙌','😎','😘','🤩'];
     
     // Inizializza il gioco
     function initGame() {
